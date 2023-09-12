2b6bdf786b0ee9da2ba21d9fb5963cca7e0862ba
Tarif Ezaz <tarif.ezaz@gmail.com>
2023-09-12 22:03:18 +0600

    chore: add static pictures

---
+++ src/pictures/index.js
@@ -0,0 +1,37 @@
+const Pic = [
+    {
+        imageUrl: 'https://i.ibb.co/28NtxhS/Blog-Picture1.jpg',
+        title: 'Gardening: More Than A Hobby',
+    },
+
+    {
+        imageUrl: 'https://i.ibb.co/ZRj7g82/blog-Picture2.jpg',
+        title: 'Make Compost At Home',
+    },
+
+    {
+        imageUrl: 'https://i.ibb.co/h9VYYpW/rsz-blogpicture3.jpg',
+        title: 'Indoor Plants Can Be The Air Purifier',
+    },
+
+    {
+        imageUrl: 'https://i.ibb.co/Sfn8N2R/Blog-Picture4.jpg',
+        title: 'Plant Flowers Improve the Quality Of Environment',
+    },
+
+    {
+        imageUrl: 'https://i.ibb.co/VxsvjZZ/Blog-Picture5.jpg',
+        title: 'Medicinal Plants to Keep At Your Home Always',
+    },
+
+
+    {
+        imageUrl: 'https://i.ibb.co/m6snrg3/Blog-Picture6.jpg',
+        title: 'Grow Vegetables At Home',
+    },
+    
+    
+  ];
+  
+  export default Pic;
+  
